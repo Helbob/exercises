710f7786bbf5e819672c27ac5c00d0d81e029861
Helbob <millehelbo@outlook.dk>
2022-09-01 10:51:06 +0200

    forloop

---
+++ capitalization3.js
@@ -0,0 +1,5 @@
+const lower = "mille";
+
+const thirdUpper = lower.toUpperCase() + lower.slice(2, 3);
+
+console.log(thirdUpper);
---
+++ forloop.js
@@ -0,0 +1,44 @@
+"use strict";
+
+/* 0-9
+for (let counter = 0; counter < 10; counter++) {
+  console.log(counter);
+} */
+//console.log(`after the loop, counter is ${counter}`);
+
+/* 1-10
+for (let counter = 1; counter < 11; counter++) {
+  console.log(counter);
+}
+*/
+
+/* 10-0 and liffoff
+for (let counter = 10; counter >= 0; counter--) {
+  console.log(counter);
+}
+console.log("liftoff");
+*/
+
+/* only odd numbers 1-19
+for (let counter = 1; counter < 20; counter += 2) {
+  console.log(counter);
+}
+*/
+
+/* doubles up 
+for (let counter = 1; counter < 16777216; counter += counter) {
+  console.log(counter);
+}
+*/
+
+/* in 3 steps
+for (let counter = 111; counter < 141; counter = counter + 3) {
+  console.log(counter);
+}
+*/
+
+/* in 10 steps
+for (let counter = 100; counter >= 0; counter = counter - 10) {
+  console.log(counter);
+}
+*/
