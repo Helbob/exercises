9967c40b59034538f382a78a6a502951ea5b3df4
Helbob <millehelbo@outlook.dk>
2022-09-05 22:35:24 +0200

    it works! hex -> rgb

--- colorconvert.js
+++ colorconvert.js
@@ -1,23 +1,15 @@
 "use strict";
-/*
-const rgbString = "(192, 13, 1)";
-const front = "rgb";
-const [r, g, b] = rgbString.split(" ");
 
-function convert() {
-  console.log(front + r + g + b);
-}
-convert();
-*/
-let testo = 255;
-let hexString = "bada55";
-let [r, g, b] = hexString.substring(0);
-r = parseInt(hexString, 16);
-g = parseInt(hexString, 16);
-b = parseInt(hexString, 16);
+function convert(hexString) {
+  let r = parseInt(hexString.substring(1, 3), 16);
+  let g = parseInt(hexString.substring(3, 5), 16);
+  let b = parseInt(hexString.substring(5, 7), 16);
 
-convert(r, g, b);
-function convert() {
-  g = parseInt(hexString, 16);
   console.log(r, g, b);
 }
+
+convert("#bada55");
+convert("#facade");
+convert("#c0ffee");
+convert("#f00b42");
+convert("#c0de25");
