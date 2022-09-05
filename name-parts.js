1f4ee9efedf2d1df2e94fd962b4b6ef77393879b
Helbob <millehelbo@outlook.dk>
2022-09-05 14:01:04 +0200

    i cheated smiler

--- name-parts.js
+++ name-parts.js
@@ -1,5 +1,16 @@
-const fullName = "Siff Mille Helbo";
+/*const fullName = "Siff Mille Helbo";
 
 const [firstName, middleName, lastName] = fullName.split(" ");
 
 console.log(firstName, middleName, lastName);
+*/
+
+const fullName = "Siff Mille Helbo";
+
+const [firstName, middleName, lastName] = fullName.split(" ");
+getNameParts(firstName, middleName, lastName);
+function getNameParts(firstName, middleName, lastName) {
+  console.log(
+    `My name is ${firstName}, and middlename is ${middleName}, and lastname is ${lastName}`
+  );
+}
