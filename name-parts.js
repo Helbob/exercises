36e2bcbcaa2f093fe692f4a83e1c74d68c0d8c4a
Helbob <millehelbo@outlook.dk>
2022-09-05 14:56:03 +0200

    does not ignore middlename

--- name-parts.js
+++ name-parts.js
@@ -5,7 +5,7 @@ const [firstName, middleName, lastName] = fullName.split(" ");
 console.log(firstName, middleName, lastName);
 */
 
-const fullName = "Siff Mille Helbo";
+/*const fullName = "Siff Mille Helbo";
 
 const [firstName, middleName, lastName] = fullName.split(" ");
 getNameParts(firstName, middleName, lastName);
@@ -14,3 +14,16 @@ function getNameParts(firstName, middleName, lastName) {
     `My name is ${firstName}, and middlename is ${middleName}, and lastname is ${lastName}`
   );
 }
+*/
+const fullyName = "Harry James Potter";
+
+function fullName(lastName, firstName, middleName) {
+  /* if (arguments === 2) {
+    console.log(firstName, lastName);
+  } else {
+    console.log(firstName, middleName, lastName);
+  } */
+}
+fullName("Potter", "Harry");
+fullName("Harry", "Potter");
+fullName("Potter", "Harry", "James");
