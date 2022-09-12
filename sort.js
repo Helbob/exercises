87d996111140b6afd676a139c8b90d578ee374ec
Helbob <millehelbo@outlook.dk>
2022-09-12 15:43:46 +0200

    comment

--- sort.js
+++ sort.js
@@ -26,15 +26,29 @@ const animals = [
   },
 ];
 
+const sortedList = sortList(animals, "desc");
+console.log(sortedList);
 //Sorts from A-Z, if you wanna reverse it change < to > or return -1 to 1 and 1 to -1
-function compareName(a, b) {
-  if (a.name < b.name) {
-    return -1;
+//Now done with closure. Closure = function within a function
+function sortList(listToSort, dir) {
+  let sortDir;
+  if (dir === "asc") {
+    sortDir = 1;
   } else {
-    return 1;
+    sortDir = -1;
   }
+  listToSort = listToSort.sort(compareName);
+
+  function compareName(a, b) {
+    if (a.name < b.name) {
+      return -1 * sortDir;
+    } else {
+      return 1 * sortDir;
+    }
+  }
+  return listToSort;
 }
 
-console.log("Before sort:", animals);
-animals.sort(compareName);
-console.log("After sort:", animals);
+//console.log("Before sort:", animals);
+//animals.sort(compareName);
+//console.log("After sort:", animals);
